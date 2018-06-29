3cba498eabfb58070329e9b50d35d3ab6141e02f
David Hopkins <dlhopkins2@gmail.com>
2018-06-29 23:11:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, album, id=nil)
+  def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @album = grade
